f23f645f643ddb8764829bcf2f7ec61250336bf4
Seokhoon Moon <6430370@naver.com>
2022-02-26 20:51:22 +0900

    220226

--- textrpg/main.js
+++ textrpg/main.js
@@ -35,13 +35,16 @@ class Game {
         xp: 50,
       },
     ];
-    this.start();
+    this.start(name);
   }
-  start() {
+  start(name) {
     $gameMenu.addEventListener("submit", this.onGameMenuInput);
     $battleMenu.addEventListener("submit", this.onBattleMenuInput);
     this.changeScreen("game");
+    this.hero = new Hero(this, name);
+    this.updateHeroStat();
   }
+
   changeScreen(screen) {
     if (screen === "start") {
       $startScreen.style.display = "block";
@@ -62,6 +65,17 @@ class Game {
     const input = event.target["menu-input"].value;
     if (input === "1") {
       this.changeScreen("battle");
+      const randomIndex = Math.floor(Math.random() * this.monsterList.length);
+      const randomMonster = this.monsterList[randomIndex];
+      this.monster = new Monster(
+        this,
+        randomMonster.name,
+        randomMonster.hp,
+        randomMonster.att,
+        randomMonster.xp
+      );
+      this.updateMonsterStat();
+      this.showMessage(`몬스터와 마주쳤다. ${this.monster.name}인 것 같다!`);
     } else if (input === "2") {
     } else if (input === "3") {
     }
@@ -74,6 +88,37 @@ class Game {
     } else if (input === "3") {
     }
   };
+  updateHeroStat() {
+    const { hero } = this;
+    if (hero === null) {
+      $heroName.textContent = "";
+      $heroLevel.textContent = "";
+      $heroHp.textContent = "";
+      $heroXp.textContent = "";
+      $heroAtt.textContent = "";
+      return;
+    }
+    $heroName.textContent = hero.name;
+    $heroLevel.textContent = `${hero.lev}Lev`;
+    $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
+    $heroXp.textContent = `XP: ${hero.xp}/${15 * hero.lev}`;
+    $heroAtt.textContent = `ATT: ${hero.att}`;
+  }
+  updateMonsterStat() {
+    const { monster } = this;
+    if (monster === null) {
+      $monsterNmae.textContent = "";
+      $monsterHp.textContent = "";
+      $monsterAtt.textContent = "";
+      return;
+    }
+    $monsterNmae.textContent = monster.name;
+    $monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
+    $monsterAtt.textContent = `ATT: ${monster.att}`;
+  }
+  showMessage(text) {
+    $message.textContent = text;
+  }
 }
 
 class Hero {
@@ -103,48 +148,46 @@ $startScreen.addEventListener("submit", (event) => {
   event.preventDefault();
   const name = event.target["name-input"].value; // input에 입력한 값
   game = new Game(name);
-  $startScreen.style.display = "none";
-  $gameMenu.style.display = "block";
-  $heroName.textContent = name;
-  $heroLevel.textContent = `${hero.lev}Lev`;
-  $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
-  $heroXp.textContent = `XP: ${hero.xp}/${15 * hero.lev}`;
-  $heroAtt.textContent = `ATT: ${hero.att}`;
-  hero.name = name;
+  // $startScreen.style.display = "none";
+  // $gameMenu.style.display = "block";
+  // $heroName.textContent = name;
+  // $heroLevel.textContent = `${hero.lev}Lev`;
+  // $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
+  // $heroXp.textContent = `XP: ${hero.xp}/${15 * hero.lev}`;
+  // $heroAtt.textContent = `ATT: ${hero.att}`;
+  // hero.name = name;
 });
 
-$gameMenu.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const input = event.target["menu-input"].value;
-  if (input === "1") {
-    $gameMenu.style.display = "none";
-    $battleMenu.style.display = "block";
-    monster = JSON.parse(
-      // parse - 문자열을 객체로
-      JSON.stringify(
-        // stringify - 객체를 문자열로
-        // parse + stringify메서드를 조합해서 사용하면 대상 객체를 깊은복사(deep copy) 할 수 있다.
-        monsterList[Math.floor(Math.random() * monsterList.length)]
-      )
-    );
-    monster.maxHp = monster.hp;
-    $monsterNmae.textContent = monster.name;
-    $monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
-    $monsterAtt.textContent = `ATT: ${monster.att}`;
-    $message.textContent = `${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`;
-  } else if (input === "2") {
-  } else if (input === "3") {
-  }
-  //   const monster1 = JSON.parse(JSON.stringify(monsterList[0]));
-  //   // monster1에 monsterList[0]의 객체를 복사한 것
-  //   const monster2 = monsterList[0];
-  //   monster1.name = "새 몬스터";
-  //   console.log(monster1.name); // 새 몬스터
-  //   console.log(monsterList[0].name); // 슬라임
-  //   monster2.name = "새 몬스터";
-  //   console.log(monsterList[0].name); // 새 몬스터
-  //   console.log(monsterList[0] === monster1); // false 깊은 복사
-  //   console.log(monsterList[0] === monster2); // true 참조관계
-});
-
-// p368
+// $gameMenu.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const input = event.target["menu-input"].value;
+//   if (input === "1") {
+//     $gameMenu.style.display = "none";
+//     $battleMenu.style.display = "block";
+//     monster = JSON.parse(
+//       // parse - 문자열을 객체로
+//       JSON.stringify(
+//         // stringify - 객체를 문자열로
+//         // parse + stringify메서드를 조합해서 사용하면 대상 객체를 깊은복사(deep copy) 할 수 있다.
+//         monsterList[Math.floor(Math.random() * monsterList.length)]
+//       )
+//     );
+//     monster.maxHp = monster.hp;
+//     $monsterNmae.textContent = monster.name;
+//     $monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
+//     $monsterAtt.textContent = `ATT: ${monster.att}`;
+//     $message.textContent = `${hero.att}의 데미지를 주고, ${monster.att}의 데미지를 받았다.`;
+//   } else if (input === "2") {
+//   } else if (input === "3") {
+//   }
+//   const monster1 = JSON.parse(JSON.stringify(monsterList[0]));
+//   // monster1에 monsterList[0]의 객체를 복사한 것
+//   const monster2 = monsterList[0];
+//   monster1.name = "새 몬스터";
+//   console.log(monster1.name); // 새 몬스터
+//   console.log(monsterList[0].name); // 슬라임
+//   monster2.name = "새 몬스터";
+//   console.log(monsterList[0].name); // 새 몬스터
+//   console.log(monsterList[0] === monster1); // false 깊은 복사
+//   console.log(monsterList[0] === monster2); // true 참조관계
+// });
